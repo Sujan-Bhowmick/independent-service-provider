6afd48ad13142869705f505dbf3aadae144911e3
Your Name <imsujan93@gmail.com>
2022-07-16 11:36:00 +0600

    3rd commit

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,25 +4,24 @@ import sujan from '../../images/about/sujan.png'
 const About = () => {
     return (
         <div className='about'>
-            <div>
-                <h1>About Me</h1>
-                <img className= 'img-fluid' src= {sujan} alt="" />
+            <div className='image'>
+                <h1 className='mb-5'>About Me</h1>
+                <img style={{ height: '259px' }} className='img-fluid' src={sujan} alt="" />
             </div>
-            <div>
-                <p> My name is Sujan Bhowmick and I am a full time Web Developer. I am good at creating unique design and then make it as eye catche website, developing rich user experience and coding with JavaScript. I am always looking for something interesting and beautiful</p>
+            <div className='skills'>
+                <p className='mt-2'> My name is Sujan Bhowmick and I am a full time Web Developer. I am good at creating unique design and then make it as eye catching website, developing rich user experience and coding with JavaScript. I am always looking for something interesting and beautiful.</p>
                 <h3>Skills</h3>
-                <button className='w-50'>html</button>
-                <button className='w-50'>css</button>
-                <button className='w-50'>html5</button>
-                <button className='w-50'>css3</button>
-                <button className='w-50'>html</button>
-                <button className='w-50'>html</button>
-                <button className='w-50'>html</button>
-                <button className='w-50'>html</button>
-                <button className='w-50'>html</button>
-                <button className='w-50'>html</button>
-                <button className='w-50'>html</button>
-                
+                <span className=''>
+                    <button className='w-25 m-2 p-1 btn btn-dark'>HTML</button>
+                    <button className='w-25 m-2 p-1 btn btn-dark '>CSS</button>
+                    <button className='w-25 m-1 p-1 btn btn-dark '>HTML5</button>
+                    <button className='w-25 m-1 p-1 btn btn-dark'>css3</button>
+                    <button className='w-25 m-1 p-1 btn btn-dark'>Bootstrap</button>
+                    <button className='w-25 m-1 p-1 btn btn-dark'>Tailwind</button>
+                    <button className='w-25 m-1 p-1 btn btn-dark'>javaScript</button>
+                    <button className='w-25 m-1 p-1 btn btn-dark'>React.js</button>
+                    <button className='w-25 m-1 p-1 btn btn-dark'>MongoDB</button>
+                </span>
             </div>
         </div>
     );
